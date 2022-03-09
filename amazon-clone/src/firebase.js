5fbd560bd4f12321fab8f6c1363f13d9b1134f5c
Alyson Williams <alysonrwilliams@gmail.com>
2022-03-08 20:24:04 -0500

    header, homepage, and some of the shopping cart

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCplslp0k2IzMiEMpmQTU-HpftA-5AOP2o",
+    authDomain: "challenge-61053.firebaseapp.com",
+    projectId: "challenge-61053",
+    storageBucket: "challenge-61053.appspot.com",
+    messagingSenderId: "651627432483",
+    appId: "1:651627432483:web:4ea2a0c3f81e7877e9bf96",
+    measurementId: "G-SCG7Q08MQF"
+  };
